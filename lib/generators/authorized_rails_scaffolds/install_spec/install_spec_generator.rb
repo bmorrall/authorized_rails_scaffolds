e8895238de22f8c56b0e41c5ac178e48fc46333c
Ben Morrall <bemo56@hotmail.com>
2013-03-21 00:01:04 +1100

    install_spec generator no longer requires a name attribute

--- lib/generators/authorized_rails_scaffolds/install_spec/install_spec_generator.rb
+++ lib/generators/authorized_rails_scaffolds/install_spec/install_spec_generator.rb
@@ -1,4 +1,4 @@
-class AuthorizedRailsScaffolds::InstallSpecGenerator < Rails::Generators::NamedBase
+class AuthorizedRailsScaffolds::InstallSpecGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
 
   def create_rspec_templates
