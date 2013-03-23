154ccb4e99e481528a723f4b2a2324b83374103e
Ben Morrall <bemo56@hotmail.com>
2013-03-23 21:30:49 +1100

    Added DeviceCanCanControllerMacro

--- lib/generators/authorized_rails_scaffolds/install_spec/install_spec_generator.rb
+++ lib/generators/authorized_rails_scaffolds/install_spec/install_spec_generator.rb
@@ -1,12 +1,26 @@
 class AuthorizedRailsScaffolds::InstallSpecGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
 
+  class_option :authentication, :type => 'string', :default => 'devise', :desc => "Authentication Provider (Devise)"
+  class_option :authorization, :type => 'string', :default => 'can_can', :desc => "Authorization Provider (CanCan)"
+  
+
   def create_rspec_templates
     copy_rspec_template 'controller_spec.rb'
     copy_rspec_template 'edit_spec.rb'
     copy_rspec_template 'index_spec.rb'
     copy_rspec_template 'new_spec.rb'
     copy_rspec_template 'show_spec.rb'
+  end
+
+  def create_controller_macros
+    parts = []
+    parts << options[:authentication].underscore unless options[:authentication].nil?
+    parts << options[:authorization].underscore unless options[:authorization].nil?
+    template_file = parts.any? ? parts.join('_') : nil
+    
+    copy_file [template_file, 'controller_macros.rb'].join('/'), "spec/support/#{template_file}_controller_macros.rb"
+    readme [template_file, 'USAGE'].join('/')
   end
 
   protected
---
+++ lib/generators/authorized_rails_scaffolds/install_spec/templates/devise_can_can/controller_macros.rb
@@ -0,0 +1,25 @@
+module DeviceCanCanControllerMacros
+  def login_unauthorized_user
+    before(:each) do
+      @ability = Object.new
+      @ability.extend(CanCan::Ability)
+      @controller.stubs(:current_ability).returns(@ability)
+
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      @logged_in_user = FactoryGirl.create(:user)
+      sign_in @logged_in_user
+    end
+  end
+  def login_user_with_ability(action, subject)
+    before(:each) do
+      @ability = Object.new
+      @ability.extend(CanCan::Ability)
+      @ability.can action, subject
+      @controller.stubs(:current_ability).returns(@ability)
+
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      @logged_in_admin = FactoryGirl.create(:user)
+      sign_in @logged_in_admin
+    end
+  end
+end
