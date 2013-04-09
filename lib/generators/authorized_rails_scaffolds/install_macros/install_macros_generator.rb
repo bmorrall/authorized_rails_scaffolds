b7a4a39c267fa49c4e0bc0e9f9ac83ecb60c9b32
Ben Morrall <bemo56@hotmail.com>
2013-04-09 21:46:11 +1000

    Reordered generators into macro and template generators

--- lib/generators/authorized_rails_scaffolds/install_macros/install_macros_generator.rb
+++ lib/generators/authorized_rails_scaffolds/install_macros/install_macros_generator.rb
@@ -1,21 +1,8 @@
-class AuthorizedRailsScaffolds::InstallSpecGenerator < Rails::Generators::Base
+class AuthorizedRailsScaffolds::InstallMacrosGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
 
   class_option :authentication, :type => 'string', :default => 'devise', :desc => "Authentication Provider (Devise)"
   class_option :authorization, :type => 'string', :default => 'can_can', :desc => "Authorization Provider (CanCan)"
-
-  def create_model_templates
-    copy_model_template 'model_spec.rb'
-  end
-
-  def create_scaffold_templates
-    copy_scaffold_template 'controller_spec.rb'
-    copy_scaffold_template 'edit_spec.rb'
-    copy_scaffold_template 'index_spec.rb'
-    copy_scaffold_template 'new_spec.rb'
-    copy_scaffold_template 'show_spec.rb'
-    copy_scaffold_template 'routing_spec.rb'
-  end
 
   def create_controller_macros
     parts = []
@@ -25,16 +12,6 @@ class AuthorizedRailsScaffolds::InstallSpecGenerator < Rails::Generators::Base
     
     copy_file [template_file, 'controller_macros.rb'].join('/'), "spec/support/#{template_file}_controller_macros.rb"
     readme [template_file, 'USAGE'].join('/')
-  end
-
-  protected
-
-  def copy_model_template(template_name)
-    copy_file template_name, "lib/templates/rspec/model/#{template_name}"
-  end
-
-  def copy_scaffold_template(template_name)
-    copy_file template_name, "lib/templates/rspec/scaffold/#{template_name}"
   end
 
 end
---
+++ lib/generators/authorized_rails_scaffolds/install_templates/install_templates_generator.rb
@@ -0,0 +1,51 @@
+class AuthorizedRailsScaffolds::InstallTemplatesGenerator < Rails::Generators::Base
+  source_root File.expand_path('../templates', __FILE__)
+
+  def copy_rails_templates
+    # Controller Templates
+    copy_rails_scaffold_controller 'controller.rb'
+
+    # View Templates (erb)
+    copy_erb_scaffold_template '_form.html.erb'
+    copy_erb_scaffold_template 'edit.html.erb'
+    copy_erb_scaffold_template 'index.html.erb'
+    copy_erb_scaffold_template 'new.html.erb'
+    copy_erb_scaffold_template 'show.html.erb'
+  end
+
+  def create_spec_templates
+    # Controller Spec Templates
+    copy_rspec_scaffold_template 'controller_spec.rb'
+
+    # Model Spec Templates
+    copy_rspec_model_template 'model_spec.rb'
+
+    # Routing Spec Templates
+    copy_rspec_scaffold_template 'routing_spec.rb'
+
+    # View Spec Templates
+    copy_rspec_scaffold_template 'edit_spec.rb'
+    copy_rspec_scaffold_template 'index_spec.rb'
+    copy_rspec_scaffold_template 'new_spec.rb'
+    copy_rspec_scaffold_template 'show_spec.rb'
+  end
+
+  protected
+
+  def copy_erb_scaffold_template(template_name)
+    copy_file "scaffold/#{template_name}", "lib/templates/erb/scaffold/#{template_name}"
+  end
+
+  def copy_rails_scaffold_controller(template_name)
+    copy_file "scaffold/#{template_name}", "lib/templates/rails/scaffold_controller/#{template_name}"
+  end
+
+  def copy_rspec_model_template(template_name)
+    copy_file "spec/#{template_name}", "lib/templates/rspec/model/#{template_name}"
+  end
+
+  def copy_rspec_scaffold_template(template_name)
+    copy_file "spec/#{template_name}", "lib/templates/rspec/scaffold/#{template_name}"
+  end
+
+end
