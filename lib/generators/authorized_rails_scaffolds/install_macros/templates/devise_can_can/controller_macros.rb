dffdea283009a7cdcbc46a219066d8a21e0c380f
Ben Morrall <bemo56@hotmail.com>
2013-04-11 23:09:59 +1000

    Controller macros are now included rather instead of being extended

--- lib/generators/authorized_rails_scaffolds/install_macros/templates/devise_can_can/controller_macros.rb
+++ lib/generators/authorized_rails_scaffolds/install_macros/templates/devise_can_can/controller_macros.rb
@@ -1,25 +1,38 @@
 module DeviseCanCanControllerMacros
-  def login_unauthorized_user
-    before(:each) do
+
+  module ClassMethods
+    def login_unauthorized_user
+      before(:each) do
+        stub_ability
+
+        @request.env["devise.mapping"] = Devise.mappings[:user]
+        @logged_in_user = FactoryGirl.create(:user)
+        sign_in @logged_in_user
+      end
+    end
+
+    def login_user_with_ability(action, subject)
+      before(:each) do
+        stub_ability.can action, subject
+
+        @request.env["devise.mapping"] = Devise.mappings[:user]
+        @logged_in_user = FactoryGirl.create(:user)
+        sign_in @logged_in_user
+      end
+    end
+  end
+
+  def self.included(controller_spec)
+    controller_spec.extend(ClassMethods)
+  end
+
+  def stub_ability
+    unless @ability
       @ability = Object.new
       @ability.extend(CanCan::Ability)
       @controller.stubs(:current_ability).returns(@ability)
-
-      @request.env["devise.mapping"] = Devise.mappings[:user]
-      @logged_in_user = FactoryGirl.create(:user)
-      sign_in @logged_in_user
     end
+    @ability
   end
-  def login_user_with_ability(action, subject)
-    before(:each) do
-      @ability = Object.new
-      @ability.extend(CanCan::Ability)
-      @ability.can action, subject
-      @controller.stubs(:current_ability).returns(@ability)
 
-      @request.env["devise.mapping"] = Devise.mappings[:user]
-      @logged_in_user = FactoryGirl.create(:user)
-      sign_in @logged_in_user
-    end
-  end
 end
