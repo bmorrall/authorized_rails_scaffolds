10ea733495c959fda9048260466063fcab384d8e
Ben Morrall <bemo56@hotmail.com>
2013-04-25 15:49:54 +1000

    Installed RSpec for testing

---
+++ spec/lib/authorized_rails_scaffolds/controller_spec_helper_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe AuthorizedRailsScaffolds::ControllerSpecHelper do
+  pending "write it"
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,8 @@
+require "bundler/setup"
+Bundler.require
+
+require 'authorized_rails_scaffolds'
+
+RSpec.configure do |config|
+  # some (optional) config here
+end
