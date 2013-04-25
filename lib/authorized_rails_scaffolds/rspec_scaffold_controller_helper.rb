ef93495e314ab6008ac4f2618c6d2de27377cf1d
Ben Morrall <bemo56@hotmail.com>
2013-04-26 00:17:12 +1000

    Moved example controller path code into routing helpers

--- lib/authorized_rails_scaffolds/rspec_scaffold_controller_helper.rb
+++ lib/authorized_rails_scaffolds/rspec_scaffold_controller_helper.rb
@@ -1,5 +1,25 @@
 class AuthorizedRailsScaffolds::RSpecScaffoldControllerHelper < AuthorizedRailsScaffolds::RSpecScaffoldHelper
 
-  
+  def create_factory_model
+    extra_params = extra_model_params
+    "FactoryGirl.create(:#{var_name}#{extra_params})"
+  end
+
+  def create_parent_model(model_name)
+    extra_params = extra_model_params(model_name)
+    "FactoryGirl.create(:#{model_name}#{extra_params})"
+  end
+
+  protected
+
+  def extra_model_params(model_name = nil)
+    argument_params = []
+    parent_model_tables.each do |parent_model|
+      attribute = parent_model.underscore
+      break if model_name == attribute
+      argument_params << ", :#{attribute} => @#{attribute}"
+    end
+    argument_params.join('')
+  end
 
 end
--- lib/authorized_rails_scaffolds/rspec_scaffold_helper.rb
+++ lib/authorized_rails_scaffolds/rspec_scaffold_helper.rb
@@ -9,68 +9,11 @@ class AuthorizedRailsScaffolds::RSpecScaffoldHelper < AuthorizedRailsScaffolds::
 
     class_name_parts = @controller_class_name.split("::")
 
-    example_parent_values = {}
-    parent_models.each_with_index do |parent_model, index|
-      example_parent_values[parent_model] = index + 2
-    end
-
-    namespace_prefix_modules = class_name_parts[0..-2] # ['Example', 'V1']
-    local_class_name = class_name_parts[-1]
-
-    parent_model_and_value_parts = []
-    parent_model_param_parts = []
-    parent_models.each do |parent_model|
-      parent_value = example_parent_values[parent_model]
-
-      # User, 2
-      parent_model_and_value_parts << parent_model.pluralize
-      parent_model_and_value_parts << parent_value
-
-      parent_model_param_parts << "#{parent_model.underscore}_id => #{parent_value}"
-    end
-
-    # Example index route: i.e. /awesome/users/2/foo_bars
-    @example_controller_path = [namespace_prefix_modules + parent_model_and_value_parts + [local_class_name.pluralize]].join("/").underscore
-
-    @example_controller_path_extra_params = parent_model_param_parts.any? ? ", :#{parent_model_param_parts.join(', :')}" : ''
-
     @attributes = options[:attributes]
-  end
-
-  def create_factory_model
-    extra_params = extra_model_params
-    "FactoryGirl.create(:#{var_name}#{extra_params})"
-  end
-
-  def create_parent_model(model_name)
-    extra_params = extra_model_params(model_name)
-    "FactoryGirl.create(:#{model_name}#{extra_params})"
   end
 
   def controller_class_name
     @controller_class_name
-  end
-
-  # Example index route: i.e. /awesome/users/2/foo_bars
-  def example_controller_path
-    "/#{@example_controller_path}"
-  end
-
-  # Extra params for an example controller path: i.e. ', :user_id => 2'
-  def example_controller_path_extra_params
-    @example_controller_path_extra_params
-  end
-
-  protected
-
-  def extra_model_params(model_name = nil)
-    argument_params = []
-    AuthorizedRailsScaffolds.config.parent_models.each do |parent_model|
-      attribute = parent_model.underscore
-      break if model_name == attribute
-      argument_params << ", :#{attribute} => @#{attribute}"
-    end
-    argument_params.join('')
   end
 
 end
