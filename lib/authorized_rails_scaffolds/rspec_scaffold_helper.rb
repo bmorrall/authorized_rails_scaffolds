0d0443e4ffa585199aba6ed99e301f1bc52a9df1
Ben Morrall <bemo56@hotmail.com>
2013-04-26 00:17:12 +1000

    Moved example controller path code into routing helpers

--- lib/authorized_rails_scaffolds/rspec_scaffold_helper.rb
+++ lib/authorized_rails_scaffolds/rspec_scaffold_helper.rb
@@ -3,12 +3,7 @@ class AuthorizedRailsScaffolds::RSpecScaffoldHelper < AuthorizedRailsScaffolds::
   def initialize(options = {})
     super options
 
-    parent_models = AuthorizedRailsScaffolds.config.parent_models
-
     @controller_class_name = options[:class_name] || options[:local_class_name]
-
-    class_name_parts = @controller_class_name.split("::")
-
     @attributes = options[:attributes]
   end
 
