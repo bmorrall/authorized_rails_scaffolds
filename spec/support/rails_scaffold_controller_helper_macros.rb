5a576b34b46971bbd8e0dd69401380c876149f06
bmorrall <ben.morrall@sitepoint.com>
2013-04-30 13:18:04 +1000

    Removed default arguments from build spec macros

--- spec/support/rails_scaffold_controller_helper_macros.rb
+++ spec/support/rails_scaffold_controller_helper_macros.rb
@@ -3,9 +3,9 @@ module RailsScaffoldControllerHelperMacros
 
   def build_rails_controller_spec_helper(options = {})
     defaults = {
-      class_name: 'Scaffold::Example', # Model class name
-      singular_table_name: 'scaffold_example', # path and model name
-      file_name: 'example', # last part of singular_table_name
+      # class_name: 'Scaffold::Example', # Model class name
+      # singular_table_name: 'scaffold_example', # path and model name
+      # file_name: 'example', # last part of singular_table_name
       attributes: [],
     }
     defaults.merge! options
--- spec/support/rspec_scaffold_controller_helper_macros.rb
+++ spec/support/rspec_scaffold_controller_helper_macros.rb
@@ -3,9 +3,9 @@ module RSpecScaffoldControllerHelperMacros
 
   def build_controller_spec_helper(args = {})
     defaults = {
-      class_name: 'Scaffold::Example', # Model class name
-      singular_table_name: 'scaffold_example', # path and model name
-      file_name: 'example', # last part of singular_table_name
+      # class_name: 'Scaffold::Example', # Model class name
+      # singular_table_name: 'scaffold_example', # path and model name
+      # file_name: 'example', # last part of singular_table_name
       attributes: [],
     }
     defaults.merge! (args)
