1501ee3299ca087630011b7824b6496d2c5d0435
Ben Morrall <bemo56@hotmail.com>
2013-05-19 13:14:15 +1000

    Added coverage for basic rails scaffold properties

--- spec/lib/authorized_rails_scaffolds/rails_scaffold_controller_helper_spec.rb
+++ spec/lib/authorized_rails_scaffolds/rails_scaffold_controller_helper_spec.rb
@@ -3,6 +3,40 @@ require 'spec_helper'
 describe AuthorizedRailsScaffolds::RailsScaffoldControllerHelper do
   include RailsScaffoldControllerHelperMacros
 
+  describe 'template variables' do
+    context 'within nested modules' do
+      let(:subject) do
+        build_rails_controller_spec_helper :class_name => 'Example::FooBar'
+      end
+      it { subject.resource_class.should eq('FooBar') }
+      it { subject.resource_human_name.should eq('Foo bar') }
+      it { subject.resource_symbol.should eq(':foo_bar') }
+      it { subject.resource_name.should eq('foo_bar') }
+      it { subject.resource_array_name.should eq('foo_bars') }
+      it { subject.resource_var.should eq('@foo_bar') }
+      it { subject.resource_array_var.should eq('@foo_bars') }
+
+      it { subject.example_controller_path.should eq("/example/foo_bars") }
+    end
+    context 'within nested modules and parent parent models' do
+      let(:subject) do
+        AuthorizedRailsScaffolds.configure do |config|
+          config.parent_models = ['Parent']
+        end
+        build_rails_controller_spec_helper :class_name => 'Example::FooBar'
+      end
+      it { subject.resource_class.should eq('FooBar') }
+      it { subject.resource_human_name.should eq('Foo bar') }
+      it { subject.resource_symbol.should eq(':foo_bar') }
+      it { subject.resource_name.should eq('foo_bar') }
+      it { subject.resource_array_name.should eq('foo_bars') }
+      it { subject.resource_var.should eq('@foo_bar') }
+      it { subject.resource_array_var.should eq('@foo_bars') }
+
+      it { subject.example_controller_path.should eq("/example/parents/2/foo_bars") }
+    end
+  end
+
   describe '#application_controller_class' do
     context 'with no parent modules' do
       it 'returns a default ApplicationController' do
